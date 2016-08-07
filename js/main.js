6962cba37af393acf3b60c5799d00b3219c735bc
ags2121 <alexsilva88@gmail.com>
2016-08-07 15:45:32 -0400

    test2

--- js/main.js
+++ js/main.js
@@ -1,40 +1,35 @@
-// var artists = [
-// 	{
-// 		name : 'Derp City Blues',
-// 		imageFile : 'b1.jpg'
-// 	},
-// 	{
-// 		name: 'CR-39',
-// 		imageFile : 'b2.jpg'
-// 	},
-// 	{
-// 		name : 'Aire Tech',
-// 		imageFile : 'b3.jpg'
-// 	}
-// ];
-// var index = 0;
-
-// document.querySelectorAll('.arrow').forEach(function (el) {
-// 	alert('load');
-// 	el.addEventListener('click', function (e) { 
-// 		alert('yo');
-// 		e.preventDefault();
-// 		if (e.currentTarget.classList.contains('left')) {
-// 			index--;
-// 			if (index < 0) {
-// 				index = artists.length-1;
-// 			}
-// 		} else {
-// 			index++;
-// 			if (index > artists.length-1) {
-// 				index = 0;
-// 			}
-// 		}
-// 		document.querySelector('.carousel .artist-name').innerHTML = artists[index].name;
-// 		document.querySelector('.carousel .artist-image').src = 'assets/' + artists[index].imageFile;
-// 	});
-// });
-
-// alert('JS?');
-
+var artists = [
+	{
+		name : 'Derp City Blues',
+		imageFile : 'b1.jpg'
+	},
+	{
+		name: 'CR-39',
+		imageFile : 'b2.jpg'
+	},
+	{
+		name : 'Aire Tech',
+		imageFile : 'b3.jpg'
+	}
+];
+var index = 0;
 document.querySelector('footer').innerHTML = "BUTT";
+
+document.querySelectorAll('.arrow').forEach(function (el) {
+	el.addEventListener('click', function (e) { 
+		e.preventDefault();
+		if (e.currentTarget.classList.contains('left')) {
+			index--;
+			if (index < 0) {
+				index = artists.length-1;
+			}
+		} else {
+			index++;
+			if (index > artists.length-1) {
+				index = 0;
+			}
+		}
+		document.querySelector('.carousel .artist-name').innerHTML = artists[index].name;
+		document.querySelector('.carousel .artist-image').src = 'assets/' + artists[index].imageFile;
+	});
+});
