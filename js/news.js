49dc2a51a5d09feae20d6b540b49f3574d865e43
ags2121 <alexsilva88@gmail.com>
2017-05-06 15:06:18 -0400

    minor edits

--- js/news.js
+++ js/news.js
@@ -2,7 +2,7 @@ var news = [
 	{
 		title: "Face of Man Announces New LP Out July 2017 on Mr. Records",
 		date: "05/11/2017",
-		body: "<em>Bouquet</em>, is Face of Man's sophomore record and the first LP release for Mr. Records."
+		body: "<em>Bouquet</em> is Face of Man's sophomore record and the first LP release for Mr. Records."
 	},
 	{
 		title: "CR-39 Signs With Mr. Records",
@@ -15,13 +15,13 @@ var news = [
 		body: "Living Situ's EP <em>Invitation to Phuket</em>, a project of \"interposed global soundscapes\", is coming in early 2018.",
 	},
 	{
-		title: "Willem Porcelain, Joydeep Thomson announce EPs \"Cratersoft\" and \"Dolores\"",
+		title: "Willem Porcelain, Joydeep Thomson announce EPs <em>Cratersoft</em> and <em>Dolores</em>",
 		date: "01/25/2017",
 		body: "Mr. Records is happy to announce the debut EPs from Willem and Joydeep. Both are slated for 2018 release."
 	},
 	{
 		title: "Say Hello to Mr. Records",
 		date: "01/01/2016",
-		body: "It's a New Year and Mr. Records is finally ready to say <em>Hello World</em>. Stay tuned for music from Joydeep Thomson, Willem Porcelain, Living Situ, and Face of Man. Please follow Mr. Records on <a href='https://twitter.com/mister_records'>Twitter</a> and <a href='https://soundcloud.com/mr-records-records'>Soundcloud</a> :)"
+		body: "It's a New Year and Mr. Records is finally ready to say <em>Hello World</em>. Stay tuned for music from Joydeep Thomson, Willem Porcelain, Living Situ, Face of Man, and more. Please follow Mr. Records on <a href='https://twitter.com/mister_records'>Twitter</a> and <a href='https://soundcloud.com/mr-records-records'>Soundcloud</a> :)"
 	}
 ]
